2e2518784070f88b4550a775079f29e0ba9a7235
Lee <120439693+lygitdata@users.noreply.github.com>
2023-11-11 17:06:04 -0500

    Update code_li_yuan.R

--- code/code_li_yuan.R
+++ code/code_li_yuan.R
@@ -8,6 +8,7 @@ library(caret)
 library(class)
 library(xgboost)
 library(pROC)
+library(e1071)
 
 findNonNAColumns = function(data) {
   return(colnames(data)[apply(data, 2, function(col) all(!is.na(col)))])
@@ -61,6 +62,29 @@ FULLDATA = FULLDATA[, -c(13:42)]
 FULLDATA = FULLDATA[, -c(79:149)]
 FULLDATA = na.omit(FULLDATA)
 
+# Import 2017 - 2018 Diabetes
+# Doc: https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/DIQ_J.htm
+currentVar = colnames(FULLDATA)
+DIQ_J = read_xpt("https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/DIQ_J.XPT")
+FULLDATA = FULLDATA %>% left_join(DIQ_J, by = "SEQN")
+FULLDATA = FULLDATA[c(currentVar, "DIQ010", "DIQ050")]
+FULLDATA = FULLDATA %>%
+  mutate(
+    DIQ010 = as.factor(DIQ010),
+    DIQ050 = as.factor(DIQ050)
+  )
+
+# Import 2017 - 2018 Health Insurance
+# Doc: https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/HIQ_J.htm
+currentVar = colnames(FULLDATA)
+HIQ_J = read_xpt("https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/HIQ_J.XPT")
+FULLDATA = FULLDATA %>% left_join(HIQ_J, by = "SEQN")
+FULLDATA = FULLDATA[c(currentVar, "HIQ011")]
+FULLDATA = FULLDATA %>%
+  mutate(
+    HIQ011 = as.factor(HIQ011)
+  )
+
 # Prepare data for training and testing
 FULLDATA = FULLDATA[,-2]
 n.obs = nrow(FULLDATA)
@@ -94,43 +118,7 @@ mean(knn.predictions == test$BPXLEVEL)
 # ROC - AUC
 multiclass.roc(as.numeric(test$BPXLEVEL) - 1, 
                as.numeric(knn.predictions) - 1)$auc
-
-# Gradient boosting with 10 fold CV
-xgb.data = xgb.DMatrix(data = as.matrix(train.X.dummy[, -1]), 
-                       label = recode(train.X.dummy$BPXLEVEL, 
-                                      '0'=0, '1'=1, '2'=2))
-xgb.test.X = data.matrix(test.X.dummy[, -1])
-
-params = list(
-  eta = 0.01,   
-  max_depth = 10,   
-  subsample = 0.7, 
-  colsample_bytree = 0.8, 
-  tree_method = "exact",
-  objective = "multi:softmax",
-  num_class = 3
-)
-
-cv.xgb = xgb.cv(
-  params = params,
-  data = xgb.data,
-  nfold = 10,
-  metrics = "merror",  
-  verbose = 0,
-  nrounds = 18 
-)
-
-eval.log = as.data.frame(cv.xgb$evaluation_log)
-min.mlogloss = min(eval.log[, 4])
-min.mlogloss.index = which.min(eval.log[, 4])
-xgb.model = xgboost(params = params, 
-                    data = xgb.data, 
-                    nrounds = min.mlogloss.index, 
-                    verbose = 0)
-xgb.predictions = predict(xgb.model, xgb.test.X)
-mean(xgb.predictions == test.Y)
-multiclass.roc(as.numeric(test.Y) - 1, 
-               as.numeric(xgb.predictions) - 1)$auc
+confusionMatrix(as.factor(knn.predictions), test.Y)
 
 # Softmax classification with 10 fold CV
 cvfit = cv.glmnet(train.X.mm, as.matrix(train.Y), family = "multinomial", 
@@ -142,3 +130,108 @@ mean(softmax.predictions == test.Y)
 softmax.rocr = multiclass.roc(as.numeric(test.Y) - 1, 
                               as.numeric(softmax.predictions) - 1)
 softmax.rocr$auc
+confusionMatrix(as.factor(softmax.predictions), test.Y)
+
+set.seed(88)
+# Gradient boosting with 10 fold CV
+xgb.data = xgb.DMatrix(data = as.matrix(train.X.dummy[, -1]), 
+                       label = recode(train.X.dummy$BPXLEVEL, 
+                                      '0'=0, '1'=1, '2'=2))
+xgb.test.X = data.matrix(test.X.dummy[, -1])
+hyperparameters = list(
+  eta = 0.0052,
+  subsample = 0.75,
+  col_subsample = 0.8,
+  max_depth = 10
+)
+   
+params = list(
+  eta = hyperparameters$eta,
+  subsample = hyperparameters$subsample,
+  colsample_bytree = hyperparameters$col_subsample,  
+  max_depth = hyperparameters$max_depth,
+  tree_method = "exact",
+  objective = "multi:softmax",
+  num_class = 3
+)
+   
+cv.xgb = xgb.cv(
+  params = params,
+  data = xgb.data,
+  nfold = 10,
+  metrics = "merror",
+  verbose = 0,
+  nrounds = 18
+)
+  
+eval.log = as.data.frame(cv.xgb$evaluation_log)
+min.mlogloss = min(eval.log[, 4])
+min.mlogloss.index = which.min(eval.log[, 4])
+xgb.model = xgboost(params = params, 
+                    data = xgb.data, 
+                    nrounds = min.mlogloss.index, 
+                    verbose = 0)
+xgb.predictions = predict(xgb.model, xgb.test.X)
+mean(xgb.predictions == test.Y)
+multiclass.roc(as.numeric(test.Y) - 1, 
+               as.numeric(xgb.predictions) - 1)$auc
+confusionMatrix(as.factor(xgb.predictions), test.Y)
+importance.matrix = xgb.importance(colnames(train.X.dummy[, -1]), model = xgb.model)
+importance.matrix
+xgb.plot.importance(importance.matrix)
+
+threshold = sort(importance.matrix$Importance)
+for (i in 1:length(selected.features)) {
+  selected.features = importance.matrix$Feature[which(importance.matrix$Importance > threshold[i])]
+  set.seed(88)
+  # Gradient boosting with 10 fold CV
+  xgb.data = xgb.DMatrix(data = as.matrix(train.X.dummy[selected.features]), 
+                         label = recode(as.matrix(train.Y), '0'=0, '1'=1, '2'=2))
+  xgb.test.X = data.matrix(test.X.dummy[selected.features])
+  hyperparameters = list(
+    eta = 0.0052,
+    subsample = 0.75,
+    col_subsample = 0.8,
+    max_depth = 10
+  )
+     
+  params = list(
+    eta = hyperparameters$eta,
+    subsample = hyperparameters$subsample,
+    colsample_bytree = hyperparameters$col_subsample,  
+    max_depth = hyperparameters$max_depth,
+    tree_method = "exact",
+    objective = "multi:softmax",
+    num_class = 3
+  )
+     
+  cv.xgb = xgb.cv(
+    params = params,
+    data = xgb.data,
+    nfold = 10,
+    metrics = "merror",
+    verbose = 0,
+    nrounds = 18
+  )
+    
+  eval.log = as.data.frame(cv.xgb$evaluation_log)
+  min.mlogloss = min(eval.log[, 4])
+  min.mlogloss.index = which.min(eval.log[, 4])
+  xgb.model = xgboost(params = params, 
+                      data = xgb.data, 
+                      nrounds = min.mlogloss.index, 
+                      verbose = 0)
+  xgb.predictions = predict(xgb.model, xgb.test.X)
+  accuracy = mean(xgb.predictions == test.Y)
+  auc = multiclass.roc(as.numeric(test.Y) - 1, 
+                      as.numeric(xgb.predictions) - 1)$auc
+  cat("Threshold:", threshold[i], "Accuracy:", accuracy, "AUC:", auc, "\n")
+}
+
+# SVM
+svm.model = svm(BPXLEVEL ~ ., data = train.X.dummy, kernel = "radial")
+svm.predictions = predict(svm.model, test.X.dummy)
+mean(svm.predictions == test.Y)
+multiclass.roc(as.numeric(test.Y) - 1, 
+               as.numeric(svm.predictions) - 1)$auc
+confusionMatrix(as.factor(svm.predictions), test.Y)
